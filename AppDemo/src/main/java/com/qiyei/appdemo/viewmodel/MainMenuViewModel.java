a53a6ecf8d172c8c10090901686de6f7f4af5961
qiyei2015 <1273482124@qq.com>
2018-06-06 20:48:57 +0800

    Cordova 引入AS

--- AppDemo/src/main/java/com/qiyei/appdemo/viewmodel/MainMenuViewModel.java
+++ AppDemo/src/main/java/com/qiyei/appdemo/viewmodel/MainMenuViewModel.java
@@ -38,12 +38,12 @@ public class MainMenuViewModel extends ViewModel{
     private String[] names = new String[]{"测试1","测试2 对话框测试","测试3 ViewPager测试","测试4 RecyclerViewTest","测试5 EasyJokeMain"
             ,"测试6 换肤测试","测试7 Banner测试","测试8 图片选择器测试","测试9 动态代理","测试10 数据中心"
             ,"测试11 测试异常信息","测试12 进程保活","测试13 Binder测试","测试14 网络框架测试","测试15 数据库框架测试"
-            ,"测试16 加密测试","测试17 Android架构组件","测试18 Rxjava测试","测试19 Okio测试5","测试20 组件化测试"};
+            ,"测试16 加密测试","测试17 Android架构组件","测试18 Rxjava测试","测试19 Okio测试5","测试20 组件化测试","测试21 Cordova测试","测试22 React Native测试"};
 
     private Class<?>[] clazzs = new Class[]{TestActivity.class,null,ViewPagerTestActivity.class,RecyclerViewTestActivity.class,EasyJokeMainActivity.class
             ,SkinTestActivity.class,BannerTestActivity.class,ImageSelectedTestActivity.class,null,DataCenterTestActivity.class
             ,null,null,BinderTestActivity.class,NetworkTestActivity.class,DatabaseTestActivity.class
-            ,EncryptActivity.class,ArchitectureActivity.class,RxjavaTestActivity.class,OkioTestActivity.class, ComponentDemoActivity.class};
+            ,EncryptActivity.class,ArchitectureActivity.class,RxjavaTestActivity.class,OkioTestActivity.class, ComponentDemoActivity.class,};
 
 
     private MutableLiveData<List<MainMenu>> mLiveData;
---
+++ CordovaDemo/src/main/java/com/qiyei/cordova/EmbeddedWebViewActivity.java
@@ -0,0 +1,86 @@
+package com.qiyei.cordova;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import org.apache.cordova.ConfigXmlParser;
+import org.apache.cordova.CordovaActivity;
+import org.apache.cordova.CordovaInterfaceImpl;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaWebViewImpl;
+import org.apache.cordova.PluginManager;
+import org.apache.cordova.engine.SystemWebView;
+import org.apache.cordova.engine.SystemWebViewEngine;
+import org.json.JSONException;
+
+/**
+ * @author Created by qiyei2015 on 2018/6/1
+ * @version: 1.0
+ * @email: 1273482124@qq.com
+ * @description: 需要继承CordovaActivity
+ */
+public class EmbeddedWebViewActivity extends CordovaActivity {
+
+    private static final String TAG = "EmbeddedWebViewActivity";
+
+    private CordovaWebView mWebInterface;
+
+    private CordovaInterfaceImpl mCordovaInterfaceImpl;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_embedded_web_view);
+
+        //初始化webview
+        ConfigXmlParser parser = new ConfigXmlParser();
+        parser.parse(this);
+
+        mCordovaInterfaceImpl = new CordovaInterfaceImpl(this);
+
+        SystemWebView systemWebView = (SystemWebView) findViewById(R.id.system_webview);
+        //初始化CordovaWebView对象
+        mWebInterface = new CordovaWebViewImpl(new SystemWebViewEngine(systemWebView));
+        //初始化mWebInterface
+        mWebInterface.init(mCordovaInterfaceImpl,parser.getPluginEntries(),parser.getPreferences());
+
+        //加载默认的url "file:///android_asset/www/index.html";
+        systemWebView.loadUrl(parser.getLaunchUrl());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        //销毁掉插件管理器
+        PluginManager pluginManager = mWebInterface.getPluginManager();
+        if (pluginManager != null){
+            pluginManager.onDestroy();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        //处理onActivityResult返回
+        mCordovaInterfaceImpl.onActivityResult(requestCode,resultCode,intent);
+    }
+
+    /**
+     * 权限回调交由 mCordovaInterfaceImpl 处理
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        try {
+            mCordovaInterfaceImpl.onRequestPermissionResult(requestCode,permissions,grantResults);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
