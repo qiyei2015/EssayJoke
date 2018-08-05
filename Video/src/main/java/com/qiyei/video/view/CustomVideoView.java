ba6c1d6c771f842ece96f91f3dc12e1e5ec45f73
qiyei2015 <1273482124@qq.com>
2018-08-05 22:08:47 +0800

    zxing二维码扫描初步集成02          添加扫描边框，修改扫描边框大小，          使之扫描线移动          增加自定义播放器类

--- Video/src/main/java/com/qiyei/video/view/CustomVideoView.java
+++ Video/src/main/java/com/qiyei/video/view/CustomVideoView.java
@@ -1,12 +1,29 @@
 package com.qiyei.video.view;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.SurfaceTexture;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.util.AttributeSet;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+
+import com.qiyei.video.R;
+
 
 /**
  * @author Created by qiyei2015 on 2018/8/1.
@@ -18,20 +35,178 @@ public class CustomVideoView extends RelativeLayout implements View.OnClickListe
         ,MediaPlayer.OnPreparedListener,MediaPlayer.OnErrorListener,MediaPlayer.OnCompletionListener
         ,MediaPlayer.OnBufferingUpdateListener,TextureView.SurfaceTextureListener{
 
+    private static final String TAG = "CustomVideoView";
+
+    private static final int TIME_MSG = 0x01;
+    private static final int TIME_INVAL = 1000;
+    /**
+     * 重试次数
+     */
+    private static final int LOAD_TOTAL_COUNT = 3;
+
+    private static final float VIDEO_HEIGHT_PERCENT = 0.5625f;
+
+    /**
+     * 生命周期
+     */
+    enum LifeCycle{
+        IDLE,PLAY,PAUSE,ERROR
+    }
+
+
+    private Context mContext;
+    /**
+     * 父布局容器
+     */
+    private ViewGroup mParentContainer;
+    /**
+     *
+     */
+    private RelativeLayout mPlayerView;
+    /**
+     * 播放器View
+     */
+    private TextureView mVideoView;
+    /**
+     * 最小化按钮？
+     */
+    private Button mMiniPlayBtn;
+    /**
+     * 全屏按钮
+     */
+    private ImageView mFullBtn;
+    /**
+     * 进度条
+     */
+    private ImageView mLoadingBar;
+    /**
+     * ?
+     */
+    private ImageView mFrameView;
+    /**
+     * 音频管理器
+     */
+    private AudioManager mAudioManager;
+    /**
+     * 媒体播放器
+     */
+    private MediaPlayer mMediaPlayer;
+    /**
+     * video
+     */
+    private Surface videoSurface;
+
+    /**
+     * 播放视频源地址
+     */
+    private String mUri;
+    /**
+     *
+     */
+    private String mFrameURI;
+    /**
+     * 是否静音
+     */
+    private boolean isMute;
+    /**
+     * 屏幕宽度
+     */
+    private int mScreenWidth;
+    /**
+     * 目标高度？
+     */
+    private int mDestationHeight;
+
+
+    /**
+     * 是否能播放
+     */
+    private boolean canPlay = true;
+    /**
+     * 是否真正暂停
+     */
+    private boolean isRealPause;
+    /**
+     * 是否完成
+     */
+    private boolean isComplete;
+    /**
+     * 当前进度
+     */
+    private int mCurrentCount;
+    /**
+     * 播放状态
+     */
+    private LifeCycle playerState = LifeCycle.IDLE;
+
+
+    /**
+     * 视频播放监听器
+     */
+    private VideoPlayerListener mListener;
+
+    /**
+     * 屏幕事件监听
+     */
+    private ScreenEventReceiver mScreenReceiver;
+
+
+    /**
+     * 进度更新处理
+     */
+    private Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case TIME_MSG:
+                    if (isPlaying()) {
+                        //正在播放,回调进度
+                        mListener.onBufferUpdate(getCurrentPosition());
+                        sendEmptyMessageDelayed(TIME_MSG,TIME_INVAL);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
 
 
-
-    public CustomVideoView(Context context) {
+    public CustomVideoView(Context context,ViewGroup parentContainer) {
         super(context);
+        mContext = context;
+        mParentContainer = parentContainer;
+        mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        initData();
+        initView();
+        registerReceiver();
     }
 
-    public CustomVideoView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+    /**
+     * 数据初始化
+     */
+    private void initData(){
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        mScreenWidth = displayMetrics.widthPixels;
+        mDestationHeight = (int)(mScreenWidth *  VIDEO_HEIGHT_PERCENT);
     }
 
-    public CustomVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    /**
+     * 初始化View
+     */
+    private void initView(){
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        mPlayerView = (RelativeLayout) inflater.inflate(R.layout.video_player_layout,this);
+        mVideoView = (TextureView) mPlayerView.findViewById(R.id.textureView);
+        mVideoView.setOnClickListener(this);
+        //屏幕常亮
+        mVideoView.setKeepScreenOn(true);
+        mVideoView.setSurfaceTextureListener(this);
+        //初始化为小布局
+        initSmallLayoutMode();
     }
 
 
@@ -52,9 +227,15 @@ public class CustomVideoView extends RelativeLayout implements View.OnClickListe
 
     @Override
     public void onPrepared(MediaPlayer mp) {
-
+        mMediaPlayer = mp;
     }
 
+    /**
+     * TextureView回调
+     * @param surface
+     * @param width
+     * @param height
+     */
     @Override
     public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
 
@@ -75,9 +256,159 @@ public class CustomVideoView extends RelativeLayout implements View.OnClickListe
 
     }
 
+    /**
+     * 点击事件
+     * @param v
+     */
     @Override
     public void onClick(View v) {
 
     }
+
+    /**
+     * 消耗事件
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return true;
+    }
+
+    /**
+     * 暂停
+     */
+    public void pause(){
+
+    }
+
+    /**
+     * 恢复播放
+     */
+    public void resume(){
+
+    }
+
+    /**
+     * 停止播放
+     */
+    public void stop(){
+
+    }
+
+
+    /**
+     * 设置是否静音
+     * @param mute
+     */
+    public void setMute(boolean mute){
+        isMute = mute;
+        if (mMediaPlayer != null && mAudioManager != null){
+            float volume = isMute ? 0.0f : 1.0f;
+            //设置左右声道音量
+            mMediaPlayer.setVolume(volume,volume);
+        }
+    }
+
+    /**
+     * 决定是否可以开始播放
+     */
+    private void decideCanPlay(){
+
+    }
+
+
+    /**
+     * 是否正在播放
+     * @return
+     */
+    private boolean isPlaying(){
+
+        return true;
+    }
+
+    /**
+     * 获取当前位置
+     * @return
+     */
+    private int getCurrentPosition(){
+
+        return 0;
+    }
+
+    /**
+     * 初始化为小布局
+     */
+    private void initSmallLayoutMode(){
+
+    }
+
+    /**
+     * 初始化广播接收器
+     */
+    private void registerReceiver() {
+        if (mScreenReceiver == null) {
+            mScreenReceiver = new ScreenEventReceiver();
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_SCREEN_OFF);
+            //解锁唤醒广播
+            filter.addAction(Intent.ACTION_USER_PRESENT);
+            mContext.registerReceiver(mScreenReceiver, filter);
+        }
+    }
+
+    /**
+     * 取消个广播接收器
+     */
+    private void unRegisterReceiver() {
+        if (mScreenReceiver != null) {
+            getContext().unregisterReceiver(mScreenReceiver);
+        }
+    }
+
+
+
+    /**
+     * 监听锁屏事件的广播接收器
+     */
+    private class ScreenEventReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //主动锁屏时 pause, 主动解锁屏幕时，resume
+            switch (intent.getAction()) {
+                case Intent.ACTION_USER_PRESENT:
+                    if (playerState == LifeCycle.PAUSE) {
+                        if (isRealPause) {
+                            //手动点的暂停，回来后还暂停
+                            pause();
+                        } else {
+                            decideCanPlay();
+                        }
+                    }
+                    break;
+                case Intent.ACTION_SCREEN_OFF:
+                    if (playerState ==  LifeCycle.PLAY) {
+                        pause();
+                    }
+                    break;
+            }
+        }
+    }
+
+
+    /**
+     * 视频播放监听
+     */
+    public interface VideoPlayerListener {
+        /**
+         * buffer更新
+         * @param time
+         */
+        void onBufferUpdate(int time);
+
+
+    }
+
+
 
 }
