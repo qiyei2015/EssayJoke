11994b86669954a36a5837a7a646c43cf16b0103
qiyei2015 <1273482124@qq.com>
2020-01-08 22:59:41 +0800

    增加GLSurfaceView 的示例1

---
+++ AndroidApp/UI/src/main/java/com/qiyei/ui/ui/activity/GLSurfaceViewDemoActivity.java
@@ -0,0 +1,16 @@
+package com.qiyei.ui.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.qiyei.ui.R;
+
+public class GLSurfaceViewDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glsurface_view_demo);
+    }
+}
