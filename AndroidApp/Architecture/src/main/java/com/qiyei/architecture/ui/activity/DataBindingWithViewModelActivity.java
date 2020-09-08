315e042116a4fd6692b943b9629670f29e9b305e
xiangwei.han <xiangwei.han@faw-vw.com>
2020-09-08 17:52:00 +0800

    databinding:数据双向绑定

--- AndroidApp/Architecture/src/main/java/com/qiyei/architecture/ui/activity/DataBindingWithViewModelActivity.java
+++ AndroidApp/Architecture/src/main/java/com/qiyei/architecture/ui/activity/DataBindingWithViewModelActivity.java
@@ -5,17 +5,18 @@ import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.qiyei.architecture.R;
-import com.qiyei.architecture.databinding.ActivityDataBindingVithViewModelBinding;
+import com.qiyei.architecture.databinding.ActivityDataBindingWithViewModelBinding;
 import com.qiyei.architecture.ui.viewmodel.DataBindingTestViewModel;
 
-public class DataBindingVithViewModelActivity extends AppCompatActivity {
+public class DataBindingWithViewModelActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityDataBindingVithViewModelBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_data_binding_vith_view_model);
+        ActivityDataBindingWithViewModelBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_data_binding_with_view_model);
 
         DataBindingTestViewModel viewModel = ViewModelProviders.of(this).get(DataBindingTestViewModel.class);
         binding.setViewmodel(viewModel);
