8a8b5a4c9f5433d3416d7f44b74cd582c7981b33
qiyei2015 <1273482124@qq.com>
2020-12-08 16:16:45 +0800

    Architecture:增加viewbinding的自动绑定

--- AndroidApp/Architecture/src/main/java/com/qiyei/architecture/base/BaseActivity.java
+++ AndroidApp/Architecture/src/main/java/com/qiyei/architecture/base/BaseActivity.java
@@ -1,17 +1,42 @@
 package com.qiyei.architecture.base;
 
-import android.content.ContentProvider;
-import android.content.ContentResolver;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewbinding.ViewBinding;
 
-public class BaseActivity extends AppCompatActivity {
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 
+public class BaseActivity<VB extends ViewBinding> extends AppCompatActivity {
+
+    protected VB mBinding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        onBeforeSetContentView();
+        Type type = getClass().getGenericSuperclass();
+        if (type instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) type;
+            Class<?> clazz = (Class<?>) parameterizedType.getActualTypeArguments()[0];
+            try {
+                Method method = clazz.getDeclaredMethod("inflate", LayoutInflater.class);
+                mBinding = (VB) method.invoke(null, getLayoutInflater());
+                setContentView(mBinding.getRoot());
+            } catch (NoSuchMethodException | IllegalAccessException| InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
+    protected void onBeforeSetContentView(){
+
+    }
+
+
 }
